ac3f1b4df0dedc95e62f48e074cbbd53975528a8
Mahbub2027 <alam201127@gmail.com>
2023-08-21 22:57:02 +0600

    cart section updated

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 let totalPrice = 0;
 function handleClickbtn(data){
     const selectedItemContainer = document.getElementById("selected-items");
-    const itemName = data.childNodes[3].innerText.split(/(\d+\.\d+)/)[0]; //[0].split(/(\d+\.\d+)/);
-    //console.log(itemName);
+    const itemName = data.childNodes[3].innerText.split(/(\d+\.\d+)/)[0]; 
+    
     const ol = document.createElement('ol');
     ol.innerText = itemName;
     selectedItemContainer.appendChild(ol);
@@ -11,43 +11,65 @@ function handleClickbtn(data){
     document.getElementById("total-price").innerText = totalPrice.toFixed(2);
 
 
+    const applyButton = document.getElementById('btn-apply');
+    if(totalPrice >= 200 ){
+        applyButton.removeAttribute('disabled');
+    }
+    else{
+        applyButton.setAttribute('disabled', true)
+    }  
+    
+    const purchaseButton = document.getElementById('btn-purchase');
+    if(totalPrice >= 0 ){
+        purchaseButton.removeAttribute('disabled');
+    }
+    else{
+        purchaseButton.setAttribute('disabled', true)
+    }  
+    
+    
 
     document.getElementById('btn-home').addEventListener('click', function(){
+        
+        location.reload();
         selectedItemContainer.innerText= "";
-        // totalPrice.value = "";
     })
+
 }
 
 
+/* document.getElementById('cuppon-input').addEventListener('keyup', function(event){
+    const cupponField = event.target.value;
+    const applyButton = document.getElementById('btn-apply');
+    if(cupponField === 'SELL200'){
+        applyButton.removeAttribute('disabled');
+    }
+    else{
+        applyButton.setAttribute('disabled', true)
+    }
+}) */
 
 
+document.getElementById('total-price').addEventListener('keyup', function(){
+    // const total = document.getElementById('total-price')
+    const totalPrice = document.getElementById('total-price')
+    const applyButton = document.getElementById('btn-purchase');
+    if(totalPrice >= 200){
+        applyButton.removeAttribute('disabled');
+    }
+    else{
+        applyButton.setAttribute('disabled', true)
+    }  
+})
 
 
+document.getElementById('btn-apply').addEventListener('click', function(){
+    const discountAmount = totalPrice * 0.20;
+    document.getElementById('discount-price').innerText = discountAmount.toFixed(2);
+    
+    const finalAmount = totalPrice - discountAmount;
+    document.getElementById('total').innerText = finalAmount.toFixed(2);
 
 
+})
 
-
-
-
-
-
-
-
-
-
-
-/* function handleClickbtn(data){
-    console.log(data.childNodes[3].innerText);
-} */
-
-/* 
-function handleCLikBtn(target) {
-    const selectedItemContainer = document.getElementById("selected-items");
-    const itemName = target.parentNode.childNodes[1].innerText;
-    const li = document.createElement("li");
-    li.innerText = itemName;
-    selectedItemContainer.appendChild(li);
-    const price = target.parentNode.childNodes[5].innerText.split(" ")[1];
-    total = parseInt(total) + parseInt(price);
-    document.getElementById("total").innerText = total;
-  } */
