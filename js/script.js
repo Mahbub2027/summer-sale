1d4bb75a5aee4c6fa6e4fe0f1359ede8172a164f
Mahbub2027 <alam201127@gmail.com>
2023-08-21 19:40:41 +0600

    cart section added

--- js/script.js
+++ js/script.js
@@ -1,1 +1,53 @@
-console.log('Hello');
+let totalPrice = 0;
+function handleClickbtn(data){
+    const selectedItemContainer = document.getElementById("selected-items");
+    const itemName = data.childNodes[3].innerText.split(/(\d+\.\d+)/)[0]; //[0].split(/(\d+\.\d+)/);
+    //console.log(itemName);
+    const ol = document.createElement('ol');
+    ol.innerText = itemName;
+    selectedItemContainer.appendChild(ol);
+    const price = data.childNodes[3].innerText.split(/(\d+\.\d+)/)[1];
+    totalPrice = parseFloat(totalPrice) + parseFloat(price);
+    document.getElementById("total-price").innerText = totalPrice.toFixed(2);
+
+
+
+    document.getElementById('btn-home').addEventListener('click', function(){
+        selectedItemContainer.innerText= "";
+        // totalPrice.value = "";
+    })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* function handleClickbtn(data){
+    console.log(data.childNodes[3].innerText);
+} */
+
+/* 
+function handleCLikBtn(target) {
+    const selectedItemContainer = document.getElementById("selected-items");
+    const itemName = target.parentNode.childNodes[1].innerText;
+    const li = document.createElement("li");
+    li.innerText = itemName;
+    selectedItemContainer.appendChild(li);
+    const price = target.parentNode.childNodes[5].innerText.split(" ")[1];
+    total = parseInt(total) + parseInt(price);
+    document.getElementById("total").innerText = total;
+  } */
